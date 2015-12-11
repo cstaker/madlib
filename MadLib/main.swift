3c046a135c855d1e071beee9ff774c224e34901e
Cody Staker <cstaker@codys-macbook-pro.local>
2015-12-11 10:32:39 -0800

    Initial Commit

---
+++ MadLib/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MadLib
+//
+//  Created by Cody Staker on 12/11/15.
+//  Copyright © 2015 Cody Staker. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
